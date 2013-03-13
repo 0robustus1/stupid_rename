144d1e704277633da4207d32ec8a8b6b51256f2b
robustus <robustus@rightsrestricted.com>
2013-03-13 22:08:33 +0100

    use clojopts to parse options
    - this also implements explicitly
      using dry-run mode
    - integrate new function for working
      on supplied files.

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [ [org.clojure/clojure "1.4.0"]
-                  [me.raynes/fs "1.4.0"] ]
+                  [me.raynes/fs "1.4.0"]
+                  [clojopts "0.3.2"] ]
   :main stupid-rename.core)
--- src/stupid_rename/core.clj
+++ src/stupid_rename/core.clj
@@ -1,6 +1,9 @@
 (ns stupid-rename.core
   ; (:gen-class))
-  (use [me.raynes.fs :only (file? directory? base-name parent absolute-path list-dir)])
+  (use [me.raynes.fs :only (file? directory?
+                            base-name parent absolute-path
+                            list-dir)]
+       clojopts.core )
   (require [clojure.string :as sfn]))
 
 ; Matches stupid numbering convention used by Stud.IP when
@@ -9,24 +12,35 @@
 
 (declare handleOptions
          shorten
+         forFiles
          printConversion)
 
 (defn -main
   "I just pass my options to handleOptions and... let the magic happen!"
-  [& args]
-  (handleOptions args))
+  [& argv]
+  (handleOptions argv))
 
 (defn handleOptions
   "I handle cmdline options."
-  [args]
-  (doseq [arg args]
-    (if (file? arg)
-      (printConversion arg)
-      (if (directory? arg)
-        (do
-          (doseq [file (list-dir arg)]
-              (printConversion (absolute-path file)) ))
-        (println "argument is not a file...") ))))
+  [argv]
+  (let [opts (clojopts "stupid_rename"
+                argv
+                (no-arg dryrun n "Do not perform actions -- dry run") )
+        files (:clojopts/more opts)]
+    (println opts)
+    (if (contains? opts :dryrun)
+      (forFiles files printConversion)
+      (forFiles files (fn [f] (println "not implemented..."))) )))
+
+(defn forFiles
+  "I will do something for every file provided."
+  [files function]
+  (doseq [file files]
+    (if (file? file)
+      (function file)
+      (if (directory? file)
+        (doseq [child_file (list-dir file)]
+          (function (absolute-path child_file)) )))))
 
 (defn printConversion
   "I will print the old filename alongside the new one..."
