de2421ddcccaa83002f60f221105dd577a925c17
robustus <robustus@rightsrestricted.com>
2013-03-13 14:14:17 +0100

    initial project commit.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject stupid_rename "0.1.0-SNAPSHOT"
+  :description "renames stupid Stud.IP bulk files (usually from a .zip-file)"
+  :url "http://example.com/FIXME"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [ [org.clojure/clojure "1.4.0"]
+                  [me.raynes/fs "1.4.0"] ]
+  :main stupid-rename.core)
---
+++ test/stupid_rename/core_test.clj
@@ -0,0 +1,7 @@
+(ns stupid-rename.core-test
+  (:use clojure.test
+        stupid-rename.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
